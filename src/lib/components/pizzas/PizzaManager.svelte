1cb5790ddbc5f44c2b38fb0a83f1ac186677f668
Ben Crisman <benjamin.crisman@gmail.com>
2022-07-31 15:23:30 -0700

    filtering additional pizzas

--- src/lib/components/pizzas/PizzaManager.svelte
+++ src/lib/components/pizzas/PizzaManager.svelte
@@ -20,12 +20,24 @@
     }
 
     function addPizza() {
-        if (pizza === undefined || pizza.name === "") {
-            err = "Please name your pizza";
-            return;
-        }
+        if (!isValidPizza(pizza)) return;
 
-        err = "";
+        // const isAllowed = pizzas.every((p) => {
+        //     const matches = p.toppings.filter((t) =>
+        //         pizza.toppings.includes(t)
+        //     );
+        //     console.log(matches);
+
+        //     if (matches.length > 0) return false;
+        //     return true;
+        // });
+
+        // if (!isAllowed) {
+        //     err = "Pizza cannot have exact same toppings as another pizza";
+        //     return;
+        // }
+
+        // err = "";
         isCreatePizza = false;
         pizzas.push(pizza);
         pizzas = pizzas;
@@ -38,6 +50,40 @@
             console.log(x);
             !selection.includes(x.name);
         });
+    }
+
+    function isValidPizza(pizza) {
+        err = "";
+        if (pizza === undefined || pizza.name === "") {
+            err = "Please name your pizza";
+            return false;
+        }
+
+        if (pizzas.find((p) => p.name === pizza.name)) {
+            err = "Cannot have duplicate pizza name";
+            return false;
+        }
+
+        let areSameToppings = false;
+        for (let i = 0; i < pizzas.length; i++) {
+            const p = pizzas[i];
+            if (p.toppings.length !== pizza.toppings.length) continue;
+
+            const p1 = p.toppings.concat().sort();
+            const p2 = p.toppings.concat().sort();
+
+            for (let j = 0; j < p1.length; j++) {
+                if (p1[j] !== p2[j]) continue;
+                areSameToppings = true;
+                break;
+            }
+
+            if (areSameToppings) {
+                err = "Pizza cannot have exact same toppings as another pizza";
+                break;
+            }
+        }
+        return !areSameToppings;
     }
 
     function updatePizza() {}
